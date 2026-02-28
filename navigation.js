dfcd5348d67955b53ba257a67d264a6509c2de93
arturogc12 <arturo.agrogiroe@gmail.com>
2026-02-28 14:31:31 +0100

    audit: fix inspection report loading and navigation hitboxes

--- navigation.js
+++ navigation.js
@@ -22,7 +22,7 @@ function injectNavigation() {
             
             ${menuItems.map(item => `
                 <a href="${item.path}" 
-                   class="flex flex-col items-center gap-1 transition-all duration-300 group ${currentPage === item.path ? 'text-white' : 'text-white/40 hover:text-white'}">
+                   class="flex flex-col items-center justify-center w-12 h-12 rounded-2xl transition-all duration-300 group ${currentPage === item.path ? 'bg-brand/10 text-white' : 'text-white/40 hover:bg-white/5 hover:text-white'}">
                     <div class="relative">
                         <i class="ph ${item.icon} text-2xl transition-transform group-hover:scale-110"></i>
                         ${currentPage === item.path ? '<div class="absolute -bottom-2 left-1/2 -translate-x-1/2 w-1.5 h-1.5 bg-brand rounded-full shadow-[0_0_10px_#4A90E2]"></div>' : ''}
@@ -31,17 +31,17 @@ function injectNavigation() {
             `).join('')}
             
             <div class="mt-auto pt-6 border-t border-white/10 w-10 flex flex-col items-center">
-                 <button onclick="handleLogout()" class="text-white/30 hover:text-red-400 transition-colors">
+                 <button onclick="handleLogout()" class="w-12 h-12 flex items-center justify-center rounded-2xl text-white/30 hover:text-red-400 hover:bg-red-400/10 transition-all">
                     <i class="ph ph-power text-2xl"></i>
                 </button>
             </div>
         </aside>
 
         <!-- Mobile/Tablet Bottom Bar -->
-        <nav class="lg:hidden fixed bottom-6 left-1/2 -translate-x-1/2 h-16 bg-[#0f172a] border border-white/10 rounded-[24px] shadow-2xl flex items-center justify-around px-6 z-[100] w-[90%] max-w-[400px]">
+        <nav class="lg:hidden fixed bottom-6 left-1/2 -translate-x-1/2 h-16 bg-[#0f172a] border border-white/10 rounded-[24px] shadow-2xl flex items-center justify-around px-2 z-[100] w-[90%] max-w-[400px]">
             ${menuItems.map(item => `
                 <a href="${item.path}" 
-                   class="flex flex-col items-center gap-0.5 transition-all duration-300 ${currentPage === item.path ? 'text-white' : 'text-white/40'}">
+                   class="flex items-center justify-center w-14 h-14 rounded-2xl transition-all duration-300 ${currentPage === item.path ? 'text-white bg-brand/10' : 'text-white/40'}">
                     <div class="relative">
                         <i class="ph ${item.icon} text-2xl"></i>
                         ${currentPage === item.path ? '<div class="absolute -top-1 -right-1 w-1.5 h-1.5 bg-brand rounded-full shadow-[0_0_10px_#4A90E2]"></div>' : ''}
@@ -49,9 +49,9 @@ function injectNavigation() {
                 </a>
             `).join('')}
             
-            <div class="w-px h-6 bg-white/10 mx-2"></div>
+            <div class="w-px h-6 bg-white/10 mx-1"></div>
             
-            <button onclick="handleLogout()" class="text-white/30 hover:text-red-400 transition-colors">
+            <button onclick="handleLogout()" class="w-14 h-14 flex items-center justify-center rounded-2xl text-white/30 hover:text-red-400 hover:bg-red-400/10 transition-all">
                 <i class="ph ph-power text-2xl"></i>
             </button>
         </nav>
